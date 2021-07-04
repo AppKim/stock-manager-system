96fe111fba7a747ddb44878dab1c3ba381f67554
gmldnjs26 <imleesky265@gmail.com>
2021-07-04 16:34:24 +0900

    config

---
+++ front/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  // add your custom rules here
+  rules: {},
+};
