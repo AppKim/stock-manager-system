77295012f42edc54170eeddfdfc9978e34615aff
chae <darrun45@gmail.com>
2021-07-18 11:39:08 +0900

    migrate database

--- back/db/migrate/20210710040341_create_products.rb
+++ back/db/migrate/20210710040341_create_products.rb
@@ -1,13 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[6.0]
   def change
-    create_table :products do |t|
+    create_table :products, id: false do |t|
       t.integer :pr_id, primary_key: true
       t.references :pr_ca_id, null: false
       t.references :pr_br_id, null: false
       t.references :pr_us_id, null: false
-      t.string :pr_price, null: true
-      t.string :pr_barcode, null: true
-      t.string :pr_img, null: true
+      t.string :pr_price, limit: 50, null: true
+      t.string :pr_barcode, limit: 255, null: true
+      t.string :pr_img, limit: 255, null: true
       t.datetime :pr_expiration, null: false 
 
       add_foreign_key :categories, :pr_ca_id, column: :ca_id, primary_key: :ca_id
@@ -16,3 +16,5 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     end
   end
 end
+
+
--- back/db/migrate/20210710040400_create_categories.rb
+++ back/db/migrate/20210710040400_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[6.0]
   def change
-    create_table :categories do |t|
+    create_table :categories, id: false do |t|
       t.integer :ca_id, null: false
-      t.string :ca_name, null: false
+      t.string :ca_name, limit: 50, null: false
     end
   end
 end
