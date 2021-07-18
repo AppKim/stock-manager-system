cad0314a8b0f07d37e9cea2cc590e8a13cdd9519
gmldnjs26 <imleesky265@gmail.com>
2021-07-18 11:44:26 +0900

    set limit

--- back/db/migrate/20210710040413_create_brands.rb
+++ back/db/migrate/20210710040413_create_brands.rb
@@ -2,7 +2,7 @@ class CreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands, id:false do |t|
       t.integer :br_id, primary_key: true
-      t.string :br_name, null:false, comment: 'ブランド名'
+      t.string :br_name, null:false, limit:50, comment: 'ブランド名'
     end
   end
 end
