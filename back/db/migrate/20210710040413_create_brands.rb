5bef8f03919c1709b0508eeb7d1aad36a8a3d039
gmldnjs26 <imleesky265@gmail.com>
2021-07-18 10:15:12 +0900

    modify brand migrate

--- back/db/migrate/20210710040413_create_brands.rb
+++ back/db/migrate/20210710040413_create_brands.rb
@@ -1,6 +1,8 @@
 class CreateBrands < ActiveRecord::Migration[6.0]
   def change
-    create_table :brands do |t|
+    create_table :brands, id:false do |t|
+      t.integer :br_id, primary_key: true
+      t.string :br_name, null:false, comment: 'ブランド名'
     end
   end
 end
