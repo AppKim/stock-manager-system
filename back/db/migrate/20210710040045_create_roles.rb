caf864e7d698b34287d6f9d25e9d92e36135b859
AppKim <40617271+AppKim@users.noreply.github.com>
2021-07-18 11:27:44 +0900

    add migration

--- back/db/migrate/20210710040045_create_roles.rb
+++ back/db/migrate/20210710040045_create_roles.rb
@@ -1,6 +1,8 @@
 class CreateRoles < ActiveRecord::Migration[6.0]
   def change
     create_table :roles, id: false do |t|
+      t.integer :ro_id, primary_key: true
+      t.string :ro_name, null: false, limit: 10, comment: 'ロール名'
     end
   end
 end
--- back/db/migrate/20210710040326_create_users.rb
+++ back/db/migrate/20210710040326_create_users.rb
@@ -1,6 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
+    create_table :users, id: false do |t|
+      t.integer :us_id, primary_key: true
+      t.reference :us_ro_id, null: false
+      t.reference :us_co_id, null: false
+      t.string :us_login_id, unique: true, limit: 50, null: false
+      t.string :us_login_pw, limit: 50, limit: 255, null: false
+
+      add_foreign_key :roles, :us_ro_id, column: :ro_id, primary_key: :ro_id
+      add_foreign_key :companies, :us_co_id, column: :co_id, primary_key: :co_id
     end
   end
 end
