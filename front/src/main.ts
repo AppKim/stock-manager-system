7c93176f770b5266c9253817b535aacfd443af49
gmldnjs26 <imleesky265@gmail.com>
2021-07-25 15:45:19 +0900

    fix config fontawesome

--- front/src/main.ts
+++ front/src/main.ts
@@ -6,10 +6,10 @@ import "./index.css";
 
 /** FontAwesome setting  */
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faCoffee);
+library.add(fas);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
