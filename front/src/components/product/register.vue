9bbbd35309716eda5b11c86468cce827a7d18cce
gmldnjs26 <imleesky265@gmail.com>
2021-08-14 17:52:16 +0900

    commit test

--- front/src/components/product/register.vue
+++ front/src/components/product/register.vue
@@ -1,9 +1,28 @@
 <template>
-  <div class="bg-red-500 h-full w-full">this is register</div>
+  <div class="bg-red-500 h-full w-full">
+    <section>
+      <label>상품명</label>
+      <input type="text" v-model="productName" />
+
+      <input type="button" @click="registerProduct" value="등록" />
+    </section>
+  </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      productName: "",
+    };
+  },
+  methods: {
+    registerProduct() {
+      console.log(this.productName);
+    },
+  },
+});
 </script>
 
 <style scoped></style>
