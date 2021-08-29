5f22e7597d4afc5193600a053080264388bfc59a
cindyperky666 <cindyperky666@gmail.com>
2021-08-30 02:07:48 +0900

    comment delete

--- front/src/components/stock/list.vue
+++ front/src/components/stock/list.vue
@@ -54,47 +54,6 @@ export default Vue.extend({
      data: {
       items: [],
       }
-    // items: [
-    //       {st_id:'1',
-    //       st_pr_id:'1',
-    //       st_exist:'1',
-    //       pr_id:'1',
-    //       pr_ca_id:'1',
-    //       pr_br_id:'1',
-    //       pr_us_id:'1',
-    //       pr_price:"1000",
-    //       pr_barcode:
-    //       '1234567',
-    //       pr_img:null,
-    //       pr_expiration:
-    //       '2022-08-11T12:00:00.000Z'},
-    //       {st_id:'2',
-    //       st_pr_id:'2',
-    //       st_exist:'1',
-    //       pr_id:'2',
-    //       pr_ca_id:'2',
-    //       pr_br_id:'2',
-    //       pr_us_id:'2',
-    //       pr_price:"2000",
-    //       pr_barcode:
-    //       '7654321',
-    //       pr_img:null,
-    //       pr_expiration:
-    //       '2022-08-11T12:00:00.000Z'},
-    //       {st_id:'3',
-    //       st_pr_id:'3',
-    //       st_exist:'1',
-    //       pr_id:'3',
-    //       pr_ca_id:'3',
-    //       pr_br_id:'3',
-    //       pr_us_id:'3',
-    //       pr_price:"3000",
-    //       pr_barcode:
-    //       '4567890',
-    //       pr_img:null,
-    //       pr_expiration:
-    //       '2022-08-11T12:00:00.000Z'}
-    // ]
     };
   },
   created() {
