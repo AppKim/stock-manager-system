7667256a2dc8d0407e4d2fcf3513dfdf2817099b
cindyperky666 <cindyperky666@gmail.com>
2021-09-05 21:27:00 +0900

    commit

--- back/db/migrate/20210731072756_add_stocks_timestamp.rb
+++ back/db/migrate/20210731072756_add_stocks_timestamp.rb
@@ -1,5 +1,5 @@
 class AddStocksTimestamp < ActiveRecord::Migration[6.0]
-  def change_table
+  def change
     add_column :stocks, :created_at, :datetime, null: false
     add_column :stocks, :updated_at, :datetime, null: false
   end
