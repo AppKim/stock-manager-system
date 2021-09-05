19e2a4f8cec5929d211e9162975edcd637786473
cindyperky666 <cindyperky666@gmail.com>
2021-09-05 21:42:52 +0900

    commit

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, { format: 'json' } do
-    get "role" => "roles#index"
+    get 'role' => 'roles#index'
+    resources :products, only: %i[index create update]
+    get 'stock' => 'stocks#index'
     get "brand" => "brands#index"
-    resources :products
-    get "stock" => "stocks#index"
   end
 end
