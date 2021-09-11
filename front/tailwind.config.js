0a0b326c1aa603f5d0d23bb9f2142b9eb6e9e14a
gmldnjs26 <imleesky265@gmail.com>
2021-09-11 21:14:45 +0900

    config: set ts

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  mode: 'jit',
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  mode: "jit",
   darkMode: false,
   theme: {
     extend: {
       colors: {
-        primary: '#1DA1F2',
-        dark: '#1a91da',
-        light: '#a5d0f5',
-        lighter: '#D4E5F4',
+        primary: "#1DA1F2",
+        dark: "#1a91da",
+        light: "#a5d0f5",
+        lighter: "#D4E5F4",
         tableColor: {
-          bg: '#f0f2f5',
-          border: '#d4dae3',
-          hover: '#f6f7f9',
-          text: '#444444',
+          bg: "#f0f2f5",
+          border: "#d4dae3",
+          hover: "#f6f7f9",
+          text: "#444444",
         },
       },
     },
