3fbeec5d12ca67bb650cc9ffd60ea7f0761e31d3
AppKim <jskim2301@yahoo.co.jp>
2021-09-20 14:04:28 +0900

    add transaction process

--- back/app/service/api/products/create_service.rb
+++ back/app/service/api/products/create_service.rb
@@ -13,18 +13,25 @@ module Api
 
       attr_reader :params
       def add_product
-        image_url = "public/product_images/#{params[:productImage].original_filename}"
-        product = Product.new(
-          pr_us_id: 1,
-          pr_ca_id: 1,
-          pr_br_id: params[:productBrand],
-          pr_name: params[:productName],
-          pr_price: params[:productPrice],
-          pr_barcode: params[:productBarcode],
-          pr_expiration: params[:productExpiration],
-          pr_img: image_url
-        )
-        product.save!
+        if params[:productImage].present?
+          image_url = "public/product_images/#{params[:productImage].original_filename}"
+        end
+        ApplicationRecord.transaction do
+          product = Product.new(
+            pr_us_id: 1,
+            pr_ca_id: 1,
+            pr_br_id: params[:productBrand],
+            pr_name: params[:productName],
+            pr_price: params[:productPrice],
+            pr_barcode: params[:productBarcode],
+            pr_expiration: params[:productExpiration],
+            pr_img: image_url
+          )
+          product.save!
+        end
+      rescue StandardError => e
+        Rails.logger.error(e.message)
+        Rails.logger.error(e.backtrace.join("\n"))
       end
 
       def upload_image
