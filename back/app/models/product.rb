76588058702bc1452bd30717c7161b081e579bd0
cindyperky666 <cindyperky666@gmail.com>
2021-09-26 23:51:38 +0900

    commit

--- back/app/models/product.rb
+++ back/app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
     has_many :stock # 부모
-    belongs_to :brand, foreign_key: :pr_br_id, primary_key: :pr_id # 자식
+    belongs_to :brand, foreign_key: :br_id, primary_key: :pr_br_id # 자식
 end
