f751644453a8f3faddcffe7b064103bb2b2e3c49
cindyperky666 <cindyperky666@gmail.com>
2021-09-26 21:49:38 +0900

    search push

--- front/src/components/stock/search.vue
+++ front/src/components/stock/search.vue
@@ -1,9 +1,65 @@
 <template>
-  <div class="bg-blue-500 h-full w-full">this is search</div>
+  <div class="bg-white-500 h-full w-full">
+    <select name="list" class="searchList">
+      <option value="product">상품명</option>
+      <option value="product">브랜드</option>
+      <option value="product">카테고리</option>
+      <option value="product">등록자</option>
+    </select>
+    <input class="searchBar" type="text" value="" placeholder="Search" v-model="searchContent" @keyup.enter="toList" />
+
+    <button class="submitBtn" @click="toList">검색</button>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      searchContent: '',
+    }
+  },
+  methods: {
+    toList() {
+      this.searchContent = ''
+    },
+  },
+}
 </script>
 
-<style scoped></style>
+<style scoped>
+.searchBar {
+  margin-left: 20px;
+  margin-bottom: 20px;
+  background: #ffffff;
+  border: 1px solid dodgerblue;
+  border-style: groove;
+  border-radius: 5px;
+  width: 70%;
+  height: 50px;
+}
+
+.searchList {
+  border: 1px solid dodgerblue;
+  border-radius: 5px;
+  height: 50px;
+  cursor: pointer;
+}
+
+.submitBtn {
+  margin-left: 30px;
+  background-color: dodgerblue;
+  font-size: 18px;
+  color: white;
+  width: 120px;
+  height: 50px;
+  cursor: pointer;
+  border-radius: 10px;
+}
+
+.submitBtn:hover {
+  box-shadow: none;
+  color: #000066;
+  background: #ffffff;
+}
+</style>
