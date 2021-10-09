3f3220049ccf476f035a54dcb5e76fc964d95c0d
gmldnjs26 <imleesky265@gmail.com>
2021-10-09 13:37:55 +0900

    fix: update

--- back/app/service/api/products/update_service.rb
+++ back/app/service/api/products/update_service.rb
@@ -1,22 +1,33 @@
 module Api
   module Products
     class UpdateService
-      def initilalize
-        ;
+      attr_reader: params
+
+      def initilalize(params)
+        @params = params
       end
 
-      def execute(params)
+      def execute()
         @product = Product.find(params[:id])
 
-        # TODO: 이미지 변경이 있을시 기존이미지 삭제 -> 다시 저장 -> image_url 업데이트
         @product.update(
           pr_name: params[:productName],
           pr_price: params[:productPrice],
           pr_br_id: params[:productBrand],
           pr_barcode: params[:productBarcode],
           # pr_expiration: params[:productExpiration],
-          # pr_image: image_url
+          pr_image: check_image(product[:pr_img])
         )
+      end
+
+      def check_image(old_image_url)
+        new_image_url = params[:productImage].present? ? "public/product_images/#{params[:productImage].original_filename}" : ''
+        if new_image_url
+          File.binwrite(image_url, params[:productImage].read)
+        else
+          old_image_url
+        end
+          # TODO: 이미지 변경이 s있을시 기존이미지 삭제 -> 다시 저장 -> image_url 업데이트
       end
     end
   end
