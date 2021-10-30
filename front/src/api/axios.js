a88746e05faba643aaf3a52d42ca6eec202a9ebe
cindyperky666 <cindyperky666@gmail.com>
2021-10-30 14:06:51 +0900

    commit

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'http://localhost:3000/'
 
 export const axiosGet = (path, param) => {
   const requestURL = '/' + path + (param ? '/' + param : '')
-  return axios.get(requestURL, param)
+  return axios.get(requestURL)
 }
 
 export const axiosPost = (path, param) => {
