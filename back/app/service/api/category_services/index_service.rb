2684ab464252915b889014c864ee9fb82a1d8e16
gmldnjs26 <imleesky265@gmail.com>
2021-11-13 13:17:41 +0900

    feat: return category list

---
+++ back/app/service/api/category_services/index_service.rb
@@ -0,0 +1,17 @@
+module Api
+    module CategoryServices
+        class IndexService
+            def initialize
+            end
+            
+            def execute
+                get_category_list
+            end
+
+            private
+            def get_category_list
+                @category = Category.all
+            end
+        end
+    end
+end
---
+++ back/app/views/api/categories/index.json.jbuilder
@@ -0,0 +1,4 @@
+json.categories @result do |category|
+    json.id category.ca_id
+    json.name category.ca_name
+end
