895116931f97f3293a3f5e1dd70036826f70d4e6
Kim <do-heyon.kim@hotmail.com>
2021-11-14 18:18:46 +0900

    [상세정보] 백앤드 수정

--- back/app/service/api/stock_services/search_service.rb
+++ back/app/service/api/stock_services/search_service.rb
@@ -11,21 +11,24 @@ module Api
 
             private
             def serach_stock
-                key = case @params[:type]
-                    when "0" then "stocks.st_pr_id = ?"
-                    when "1" then "products.pr_ca_id = ?"
-                    when "2" then "brands.br_name = ?"
+                if @params[:query] != ''
+                    key = case @params[:type]
+                        when "0" then "stocks.st_pr_id = ?"
+                        when "1" then "products.pr_ca_id = ?"
+                        when "2" then "brands.br_name = ?"
+                    end
                 end
 
-                @result = Stock.joins(
+                @result = Stock
+                .joins(
                     product: :brand
-                ).select(
-                    'stocks.st_pr_id, 
+                ).select('
+                    stocks.st_pr_id, 
                     products.pr_ca_id, 
                     brands.br_name, 
                     products.pr_price, 
-                    count(stocks.st_pr_id) as count'
-                ).where(
+                    count(stocks.st_pr_id) as count
+                ').where(
                     key, @params[:query]
                 ).group(
                     'stocks.st_pr_id'
@@ -33,7 +36,7 @@ module Api
                 
                 p @result
                 rescue StandardError => e
-                    # result.store('message', e.message)
+                    result.store('message', e.message)
                     Rails.logger.error(e.message)
                     Rails.logger.error(e.backtrace.join("\n"))
             end
