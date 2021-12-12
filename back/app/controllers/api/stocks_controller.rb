8039bb6e420511ebd559f328e9ab05d05a00a351
AppKim <jskim2301@yahoo.co.jp>
2021-12-12 20:07:46 +0900

    change service folder name

--- back/app/controllers/api/stocks_controller.rb
+++ back/app/controllers/api/stocks_controller.rb
@@ -1,27 +1,35 @@
 module Api
-    class StocksController < ApplicationController
-        def index
-            service = Api::StockServices::IndexService.new
-            # @result = service.execute
-            # @result = Stock.find_by(st_id: 1235)
-            # @result = Stock.joins(:product).select('stocks.* , products.*')
-            @result = service.execute
-        end
-
-        def create
-            service = Api::StockServices::CreateService.new(params)
-            service.execute
-        end
-
-        def detail
-            service = Api::StockServices::DetailService.new(params)
-            @result = service.execute
-          end
-
-        def search
-            service = Api::StockServices::SearchService.new(params)
-            @result = service.execute
-        end
-
+  class StocksController < ApplicationController
+    def index
+      service = Api::Stocks::IndexService.new
+      # @result = service.execute
+      # @result = Stock.find_by(st_id: 1235)
+      # @result = Stock.joins(:product).select('stocks.* , products.*')
+      @result = service.execute
     end
+
+    def create
+      service = Api::Stocks::CreateService.new(params)
+      service.execute
+    end
+
+    def detail
+      service = Api::Stocks::DetailService.new(params)
+      @result = service.execute
+    end
+
+    def search
+      service = Api::Stocks::SearchService.new(params)
+      @res = service.execute
+      if @res[:result].present?
+        if @res[:result].eql? 'fail'
+          render json: @res, status: :internal_server_error
+        else
+          render json: @res['result'], status: :ok
+        end
+      else
+        render status: :no_content
+      end
+    end
+  end
 end
