33e886c61cc1c86ba34dc983c8b063bb5608445f
gmldnjs26 <imleesky265@gmail.com>
2022-01-03 23:51:43 +0900

    config: set alert

--- front/src/main.ts
+++ front/src/main.ts
@@ -9,23 +9,21 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import LoadingPlugin from '@/plugins/LoadingPlugin.js'
-
-// https://github.com/avil13/vue-sweetalert2
-import VueSweetalert2 from 'vue-sweetalert2'
-// If you don't need the styles, do not connect
-import 'sweetalert2/dist/sweetalert2.min.css'
-Vue.use(VueSweetalert2)
-
 library.add(fas)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 /** /FontAwesome setting  */
 
-Vue.config.productionTip = false
-
+import LoadingPlugin from '@/plugins/LoadingPlugin.js'
 Vue.use(LoadingPlugin)
+
+// https://github.com/avil13/vue-sweetalert2
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
+Vue.use(VueSweetalert2)
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
